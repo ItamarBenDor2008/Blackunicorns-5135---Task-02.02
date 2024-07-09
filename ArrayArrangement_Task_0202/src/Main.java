762cc4223be4feba8b08d49b9f71095bb07c5457
Bendi <98035470+ItamarBenDor2008@users.noreply.github.com>
2024-07-09 04:14:06 +0300

    Update Main.java

--- ArrayArrangement_Task_0202/src/Main.java
+++ ArrayArrangement_Task_0202/src/Main.java
@@ -1,25 +1,38 @@
-
-import java.util.Arrays;
+import java.util.*;
 
 public class Main 
 {
     public static void main(String[] args)
     {
+        // ניצור את הרשימה שלנו ונגדיר את רשימת השמות
+
         String[] Animals  = {"dog", "pigeon", "cat", "caterpillar", "mouse"};
+
+        // נדפיס את הרשימה שלנו לפני הסידור כדי שנוכל לראות ולהשוות
+
         System.out.println(Arrays.toString(Animals));
 
+        // נשתמש בשתי לולאות פור הראשונה  עוברת על כל איבר במערך והשנייה עוברת על כל האיברים לאחר האיבר הנוכחי
         for (int i = 0; i < Animals.length; i++) 
         {
             for (int j = i + 1; j < Animals.length; j++) 
             {
+                // בכל האיטרציה הלולאות משוות את אורכי שתי המחרוזות הנוכחיות
+
                 if (Animals[i].length() < Animals[j].length()) 
                 {
                     String x = Animals[i];
+
                     Animals[i] = Animals[j];
+
                     Animals[j] = x;
                 }
+                
+                // אם המחרוזת הראשונה קצרה יותר מהשנייה מחליפים את המקומות שלהם במערך באמצעות משתנה זמני במקרה שלנו איקס
             }
         }
+
+        // ולאחר שסידרנו נדפיס את הרשימה לאחר הסידור
 
         System.out.println(Arrays.toString(Animals));
     }
